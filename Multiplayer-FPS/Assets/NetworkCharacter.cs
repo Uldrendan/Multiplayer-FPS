eeb02fba2a88c3b40590bccd9c7389909eb1a4d1
Uldrendan <danclarke88@gmail.com>
2015-12-15 21:10:31 -0500

    Initial Commit

---
+++ Multiplayer-FPS/Assets/NetworkCharacter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkCharacter : Photon.MonoBehaviour {
+
+	Vector3 realPosition = Vector3.zero;
+	Quaternion realRotation = Quaternion.identity;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (photonView.isMine) {
+			//do nothing
+		}
+		else {
+			transform.position = Vector3.Lerp(transform.position, realPosition, 0.1f);
+			transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.1f);
+		}
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){
+		if (stream.isWriting) {
+			//our player, need to send actual position to network
+
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+		} 
+		else {
+			//someone else's player, need to receive their position and update our version of that player
+
+			realPosition = (Vector3)stream.ReceiveNext();
+			realRotation = (Quaternion)stream.ReceiveNext();
+		}
+	}
+}
---
+++ Multiplayer-FPS/Assets/NetworkManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	public GameObject standbyCamera;
+	SpawnSpot[] spawnSpots;
+
+	// Use this for initialization
+	void Start () {
+		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot> ();
+		Connect ();	
+	}
+	
+	void Connect(){
+		PhotonNetwork.ConnectUsingSettings ("MultiFPS v001");
+	}
+
+	void OnGUI(){
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+	}
+
+	void OnJoinedLobby(){
+		PhotonNetwork.JoinRandomRoom ();
+		Debug.Log ("joining random room");
+	}
+
+	void OnPhotonRandomJoinFailed(){
+		Debug.Log ("random room join failed");
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom(){
+		Debug.Log ("onjoinedroom");
+		SpawnMyPlayer ();
+	}
+
+	void SpawnMyPlayer(){
+		if (spawnSpots == null) {
+			Debug.LogError ("no spawns");
+			return;
+		}
+
+		SpawnSpot mySpawnSpot = spawnSpots [Random.Range (0, spawnSpots.Length)];
+		GameObject myPlayerGO = (GameObject) PhotonNetwork.Instantiate ("PlayerController", mySpawnSpot.transform.position, mySpawnSpot.transform.rotation, 0);
+		standbyCamera.SetActive(false);
+
+		((MonoBehaviour)myPlayerGO.GetComponent("FirstPersonController")).enabled = true;
+		myPlayerGO.transform.FindChild ("FirstPersonCharacter").gameObject.SetActive(true);
+	}
+}
